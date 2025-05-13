b96e4a9f680d187578b0b9db527888f1198129e9
Cristian Martin <cristian20martin20@gmail.com>
2025-05-13 09:27:52 +0200

    home ready, footer ready

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const productContainer = document.querySelector('.product-container');
+    const reseñasContainer = document.querySelector('.reseñas-container');
     const nxtBtn = document.querySelector('.nxt-btn');
     const preBtn = document.querySelector('.pre-btn');
     let autoScrollInterval;
   
     const startAutoScroll = () => {
       autoScrollInterval = setInterval(() => {
-        const maxScrollLeft = productContainer.scrollWidth - productContainer.clientWidth;
-        if (productContainer.scrollLeft >= maxScrollLeft) {
-          productContainer.scrollLeft = 0;
+        const maxScrollLeft = reseñasContainer.scrollWidth - reseñasContainer.clientWidth;
+        if (reseñasContainer.scrollLeft >= maxScrollLeft) {
+          reseñasContainer.scrollLeft = 0;
         } else {
-          productContainer.scrollLeft += productContainer.clientWidth / 4; // Ajusta este valor para cambiar la cantidad de desplazamiento
+          reseñasContainer.scrollLeft += reseñasContainer.clientWidth / 4; // Ajusta este valor para cambiar la cantidad de desplazamiento
         }
       }, 4000); // Ajusta la velocidad del desplazamiento automático
     };
@@ -21,27 +21,27 @@ document.addEventListener('DOMContentLoaded', function() {
   
     nxtBtn.addEventListener('click', () => {
       stopAutoScroll();
-      const maxScrollLeft = productContainer.scrollWidth - productContainer.clientWidth;
-      if (productContainer.scrollLeft >= maxScrollLeft) {
-        productContainer.scrollLeft = 0;
+      const maxScrollLeft = reseñasContainer.scrollWidth - reseñasContainer.clientWidth;
+      if (reseñasContainer.scrollLeft >= maxScrollLeft) {
+        reseñasContainer.scrollLeft = 0;
       } else {
-        productContainer.scrollLeft += productContainer.clientWidth / 1; // Ajusta este valor para cambiar la cantidad de desplazamiento
+        reseñasContainer.scrollLeft += reseñasContainer.clientWidth / 1; // Ajusta este valor para cambiar la cantidad de desplazamiento
       }
       startAutoScroll();
     });
   
     preBtn.addEventListener('click', () => {
       stopAutoScroll();
-      if (productContainer.scrollLeft <= 0) {
-        productContainer.scrollLeft = productContainer.scrollWidth - productContainer.clientWidth;
+      if (reseñasContainer.scrollLeft <= 0) {
+        reseñasContainer.scrollLeft = reseñasContainer.scrollWidth - reseñasContainer.clientWidth;
       } else {
-        productContainer.scrollLeft -= productContainer.clientWidth / 1; // Ajusta este valor para cambiar la cantidad de desplazamiento
+        reseñasContainer.scrollLeft -= reseñasContainer.clientWidth / 1; // Ajusta este valor para cambiar la cantidad de desplazamiento
       }
       startAutoScroll();
     });
   
-    productContainer.addEventListener('mouseenter', stopAutoScroll);
-    productContainer.addEventListener('mouseleave', startAutoScroll);
+    reseñasContainer.addEventListener('mouseenter', stopAutoScroll);
+    reseñasContainer.addEventListener('mouseleave', startAutoScroll);
   
     startAutoScroll();
   });
